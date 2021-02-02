df05ffb5c2828a50876b2b2a1191b54849332606
Hyevvy <hyeg0@naver.com>
2021-02-02 18:02:45 +0900

    feat: login, register

--- front/matching-developers/src/containers/auth/LoginForm.js
+++ front/matching-developers/src/containers/auth/LoginForm.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../models/auth';
+import { withRouter } from 'react-router-dom';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
-import AuthFormRegister from '../../components/auth/AuthFormRegister';
-const LoginForm = () => {
+import { check } from '../../modules/user';
+
+const LoginForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -20,10 +25,30 @@ const LoginForm = () => {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password } = form;
+    dispatch(login({ username, password }));
   };
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('오류발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('로그인성공');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
@@ -34,4 +59,4 @@ const LoginForm = () => {
     />
   );
 };
-export default LoginForm;
+export default withRouter(LoginForm);
--- front/matching-developers/src/containers/auth/RegisterForm.js
+++ front/matching-developers/src/containers/auth/RegisterForm.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../models/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -20,10 +25,38 @@ const RegisterForm = () => {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      //TO DO  오류처리
+      return;
+    }
+    dispatch(register({ username, password }));
   };
+
   useEffect(() => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('오류발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+      console.log('checkAPI성공');
+      console.log(user);
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
@@ -34,4 +67,4 @@ const RegisterForm = () => {
     />
   );
 };
-export default RegisterForm;
+export default withRouter(RegisterForm);
