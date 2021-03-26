23b293f1f526e76ddfcbb20a492f9db2f602d508
DolphaGo <adamdoha@naver.com>
2021-03-26 23:51:12 +0900

    Front (#19)
    * feat: sso github login, homepage
    
    * feat: write(Quill X)
    
    * Delete .DS_Store
    
    * Delete .env.local
    
    * Revert "merge" (#18)
    
    * Revert "Delete .env.local"
    
    This reverts commit dfe923c22538dad3fed8261904067dfc7bf435e5.
    
    * Revert "Delete .DS_Store"
    
    This reverts commit 497c27a97aee9faeae5509a7ca51fc272bc1cd24.
    
    Co-authored-by: Hyevvy <hyeg0@naver.com>
    Co-authored-by: Hyevvy <72402747+Hyevvy@users.noreply.github.com>

--- front/src/component/auth/AuthForm.js
+++ front/src/component/auth/AuthForm.js
@@ -1,77 +1,27 @@
 import React, { useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import { Input } from "antd";
 import "./AuthForm.css";
 import HomePage from "../../pages/HomePage";
+
 const AuthForm = () => {
-  const [userid, setUserid] = useState("");
-  const [password, setPassword] = useState("");
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const onChangeUserid = (e) => {
-    setUserid(e.target.value);
-  };
-
-  const ErrorMessage = styled.div`
-    color:red;
-    text-align: center
-    font-size:0.875rem;
-    margin-top:1rem;
-  `;
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const onSubmitForm = (e) => {
-    e.preventDefault();
-    console.log("onSubmit");
-    alert("로그인 요청을 보냄!");
-    setError(null);
-    setLoading(true);
-    console.log(userid, password);
+  const onClickButton = (e) => {
     axios
-      .post("/api/v1/login", {
-        userid: userid,
-        password: password,
-      })
+      .get(
+        "https://github.com/login/oauth/authorize?client_id=1c5988c24cb3ace6582f&redirect_uri=http://localhost:3000/"
+      )
       .then((res) => {
-        <HomePage />;
         console.log(res);
       })
-      .catch((e) => alert("로그인 실패"));
-    setLoading(false);
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <h3 style={{ textAlign: "center", marginBottom: "2rem" }}></h3>
-      <form onSubmit={onSubmitForm}>
-        <br />
-        <br />
-        <label> 아이디 </label>
-        <Input
-          type="text"
-          name="userid"
-          value={userid}
-          onChange={onChangeUserid}
-        />
-
-        <br />
-        <br />
-        <label> 비밀번호 </label>
-        <Input
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChangePassword}
-        />
-
-        <button style={{ display: "block", marginTop: "20px" }}>
-          {" "}
-          로그인{" "}
-        </button>
-      </form>
+      <button onClick={onClickButton}>깃허브로 로그인하기</button>
     </div>
   );
 };
--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PostListPage from "./PostListPage";
 import "./HomePage.css";
+import axios from "axios";
 const HomePage = () => {
+  const onClickLogin = (e) => {
+    axios
+      .get(
+        "https://github.com/login/oauth/authorize?client_id=1c5988c24cb3ace6582f"
+      )
+      .then((code) => {
+        console.log(code);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
-      <div className="loginregister">
-        <Link to="/login"> 로그인 </Link>
-        <Link to="/register"> 회원가입 </Link>
+      <div className="aside">
+        <div className="login">
+          <a href="/" className="logo">
+            <div className="handshake">🤝</div>{" "}
+          </a>
+          <div className="mdDiv"> Matching Developers</div>
+          <button onClick={onClickLogin}>깃허브로그인 하기</button>
+          <a
+            href="https://github.com/login/oauth/authorize?scope=user:email&client_id=1c5988c24cb3ace6582f"
+            className="buttonLogin"
+          >
+            깃허브로 로그인하기
+          </a>
+        </div>
       </div>
-      <PostListPage />
+      <div className="leftDiv">페이지 간단히 설명</div>
     </div>
   );
 };
